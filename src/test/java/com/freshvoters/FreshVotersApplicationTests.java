f90f813085021b8fc9841df0e14661f83396955e
Aiizaku <Aiizaku@DESKTOP-MT4BQIB.home>
2020-07-09 14:00:57 -0400

    initial check-in of the Freshvoters code

---
+++ src/test/java/com/freshvoters/FreshVotersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freshvoters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshVotersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
